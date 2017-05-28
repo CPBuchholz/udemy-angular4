30a8d6e0e8c157c6558bb4cd9860938117da83ba
CPBuchholz <christopher.p.buchholz@gmail.com>
2017-05-27 22:17:32 -0400

    ngIf and dynamic styling

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = '';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -21,11 +22,11 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
-    this.serverName = '';
+    this.serverCreated = true;
   }
 
-  onUpdateServerName(event: Event){
-    this.serverName = (<HTMLInputElement>event.target).value;
-  }
+  // onUpdateServerName(event: Event){
+  //   this.serverName = (<HTMLInputElement>event.target).value;
+  // }
 
 }
